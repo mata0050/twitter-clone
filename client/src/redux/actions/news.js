c1289f1dc7bc3f69475abfffe87da257e3468d8f
mata0050 <mata0050@outlook.com>
2021-12-26 20:34:44 -0500

    fixed news api route in redux

--- client/src/redux/actions/news.js
+++ client/src/redux/actions/news.js
@@ -4,20 +4,7 @@ import { GET_NEWS, GET_NEWS_FAIL } from './types';
 export const getAllNews = () => async (dispatch) => {
   try {
     // dotenv the key
-    const res = await axios.get(
-      'https://cors-anywhere.herokuapp.com/https://newsdata.io/api/1/news?apikey=pub_2945c46c381d92d811903c639caa3223ec62',
-      {
-        headers: {
-          // 'X-ACCESS-KEY': 'pub_2945c46c381d92d811903c639caa3223ec62',
-          // 'Access-Control-Allow-Origin': 'http://localhost:3000/',
-          // 'Access-Control-Request-Method': 'GET',
-          // 'Access-Control-Request-Headers': 'Origin, X-ACCESS-KEY, Accept',
-          // 'Origin': 'http://localhost:3000',
-          // 'Referrer-Policy': 'strict-origin-when-cross-origin'
-        },
-      }
-    );
-    console.log(res);
+    const res = await axios.get('/news/latest');
 
     dispatch({
       type: GET_NEWS,
