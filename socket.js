3b130eeb58e9ebd22bf178123fd98dbad2ad3395
mata0050 <mata0050@outlook.com>
2021-12-30 16:40:11 -0500

    Prepared for deployment

--- socket.js
+++ socket.js
@@ -2,7 +2,7 @@
 const io = require('socket.io')(8001, {
   cors: {
     origin: ['http://localhost:3000', 'https://tweeter-c.herokuapp.com/'],
-    headers: {'Access-Control-Allow-Origin': '*'}
+    'Access-Control-Allow-Origin': '*',
   },
 });
 
@@ -10,7 +10,7 @@ const io = require('socket.io')(8001, {
 io.on('connection', (socket) => {
   console.log(socket.id);
   socket.on('create-tweet', (tweet) => {
-    io.emit('receive-tweet',tweet);
+    io.emit('receive-tweet', tweet);
     console.log(tweet);
   });
 });
