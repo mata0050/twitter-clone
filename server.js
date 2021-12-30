07ede5245a429dd37d67f8250d5856a90740a4e6
mata0050 <mata0050@outlook.com>
2021-12-30 15:53:43 -0500

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 
 //Socket io
 require('./socket');
@@ -22,14 +23,20 @@ app.use(
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.use('/register', require('./routes/auth/register'));
 app.use('/login', require('./routes/auth/auth'));
 app.use('/tweet', require('./routes/tweet'));
 app.use('/news', require('./routes/news'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
